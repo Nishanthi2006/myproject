778aa30fdd02060562c85f48e1ff78e8118192a8
Nishanthi2006 <nishanthisugumaran2006@gmail.com>
2026-04-27 10:02:47 +0530

    Add function to calculate maximum subarray sum

--- data.py
+++ data.py
@@ -1,5 +1,12 @@
-num = [1,2,3]
-number = int("".join(map(str, num)))
-number += 1
-result = [int(x) for x in str(number)]
-print(result)
+def max_subarray(arr):
+    max_current = arr[0]
+    max_global = arr[0]
+
+    for i in range(1, len(arr)):
+        max_current = max(arr[i], max_current + arr[i])
+        max_global = max(max_global, max_current)
+
+    return max_global
+
+arr = list(map(int, input("Enter elements: ").split()))
+print("Maximum Subarray Sum:", max_subarray(arr))
